2f47ae054790894879b4edcbd3161194fbf4130a
Bence <165921343+benyo22@users.noreply.github.com>
2025-03-28 13:11:43 +0100

    Edzés folytatása gomb működik

--- frontend/src/views/menu/Menu.jsx
+++ frontend/src/views/menu/Menu.jsx
@@ -26,7 +26,7 @@ export const Menu = () => {
 
       {/* Topbar */}
       <header className="topbar">
-        {workoutData?.some((w) => !w.isCompleted) && (
+        {workoutData?.some((w) => !w.isFinished) && (
           <NavLink
             to="/workouts"
             className="bg-primary-green text-primary-white mr-5 px-2 py-2 font-semibold rounded-lg hover:bg-secondary-green transition duration-300 ease-in-out cursor-pointer hover:scale-102 dark:bg-dark-primary-green dark:hover:bg-dark-secondary-green"
